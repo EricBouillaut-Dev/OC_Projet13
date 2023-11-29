a5dc101632c8907ba073a964015864d5f3521bd8
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-11-29 17:49:59 +0100

    Ajout page d'erreur

--- front/src/pages/Error.js
+++ front/src/pages/Error.js
@@ -1,8 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+//Composant de la page d'erreur
 function Error() {
   return (
-    <>
-      <div className="error"></div>
-    </>
+    <div className="error">
+      <h2>404</h2>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+    </div>
   );
 }
+
 export default Error;
